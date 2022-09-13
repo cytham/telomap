cc0ca2af533867fabb54933c6cf480b9bc39be5a
cytham <tcy604@hotmail.com>
2022-09-13 08:31:15 +0800

    Fix dependencies

--- setup.py
+++ setup.py
@@ -24,12 +24,10 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=['pandas>=1.4.2', 'numpy>=1.22.4', 'scipy>=1.8.1', 'matplotlib>=3.5.2',
-                      'pysam>=0.19.0', 'seaborn>=0.11.2', 'natsort>=8.1.0', 'biopython>=1.79'],
+                      'pysam>=0.19.0', 'seaborn>=0.11.2', 'natsort>=8.1.0', 'biopython>=1.79', 'scikit-learn>=1.1.2'],
     python_requires='>=3.6',
     classifiers=[
         "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
