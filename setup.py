17eefb2af76ee720883d729ba3040eef805923a1
Cheng Yong Tham <25361260+cytham@users.noreply.github.com>
2024-02-26 00:08:18 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, find_namespace_packages
 import os
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
