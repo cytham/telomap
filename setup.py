9ff7dc3b8c8f43ec1ab8b7e6e16060a09be49256
Cheng Yong Tham <25361260+cytham@users.noreply.github.com>
2024-02-25 23:59:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,21 +15,24 @@ setup(
     include_package_data=True,
     scripts=['telomap/telomap'],
     url='https://github.com/cytham/telomap',
-    download_url='https://github.com/cytham/telomap/releases',
     license='gpl-3.0',
     author='Tham Cheng Yong',
     author_email='chengyong.tham@u.nus.edu',
-    description='A tool for analyzing telobait-captured long-read telomere sequencing data',
+    description='A tool for analyzing telomeres in telobait-captured or WGS long-read sequencing data',
     keywords=['telomap', 'telomere', 'pacbio', 'ont', 'oxford nanopore', 'long read'],
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=['pandas>=1.4.2', 'numpy>=1.22.4', 'scipy>=1.8.1', 'matplotlib>=3.5.2',
                       'pysam>=0.19.0', 'seaborn>=0.11.2', 'natsort>=8.1.0', 'biopython>=1.79', 'scikit-learn>=1.0.2'],
-    python_requires='>=3.6',
+    entry_points={
+        "console_scripts": [
+            "telomap=telomap.telomap:main",
+        ],
+    },
+    python_requires='>=3.8',
     classifiers=[
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Topic :: Scientific/Engineering :: Bio-Informatics"
     ]
