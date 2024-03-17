506bc7a7b3a8a1fe4728d88e0ebe590a1a727aa4
Cheng Yong Tham <25361260+cytham@users.noreply.github.com>
2024-03-17 22:05:30 +0800

    Change header to TELOMERE_END_MOTIF

--- src/telomap/integrate.py
+++ src/telomap/integrate.py
@@ -112,6 +112,6 @@ class TeloMap:
         h.append('##total_reads=%s\n' % cap.counts[0])
         h.append('##captured_reads=%s\n' % cap.counts[1])
         h.append('##captured_reads_with_motif=%s\n' % cap.counts[3])
-        h.append('#RNAME\tOLIGO\tBARCODE\tSTRAND\tMOTIF\tRLEN\tRAW_TELOMERE_LEN\tTELOMERE_LEN\tTELOMERE_END\tTRF_COUNT'
+        h.append('#RNAME\tOLIGO\tBARCODE\tSTRAND\tMOTIF\tRLEN\tRAW_TELOMERE_LEN\tTELOMERE_LEN\tTELOMERE_END_MOTIF\tTRF_COUNT'
                  '\tCHROM\tTELOMERE_START\tTELOMERE_END\tNUM_PASS\tRQUAL\n')
         return h
